f99fd7eb86756342b903e3ba0b559892e0c7dcd6
Ілля Соло <illa98994@gmail.com>
2026-03-27 20:04:38 +0100

    key pressing actions

---
+++ keyActions/program_that_holds_x_key/Main.java
@@ -0,0 +1,41 @@
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class Main {
+    public static void main(String[] stringArray) {
+        Robot robot = null;
+        try {
+            System.out.println("Pressing (HOLD) key: x");
+            robot = new Robot();
+            robot.setAutoDelay(30);
+            robot.keyPress(KeyEvent.VK_X);
+            System.out.println("Done (key is now held down).");
+
+            Files.write(
+                Path.of(".ready"),
+                new byte[0],
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING
+            );
+
+            Thread.sleep(Long.MAX_VALUE);
+
+        } catch (Throwable throwable) {
+            System.out.println("Failed: " + throwable);
+            throwable.printStackTrace(System.out);
+        } finally {
+            if (robot != null) {
+                try {
+                    robot.keyRelease(KeyEvent.VK_X);
+                    System.out.println("Released key on exit.");
+                } catch (Throwable releaseError) {
+                    System.out.println("Failed to release key: " + releaseError);
+                    releaseError.printStackTrace(System.out);
+                }
+            }
+        }
+    }
+}
